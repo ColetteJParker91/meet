0d1ab646fdc80b8f48dc62bb3618fb43d1dd01d3
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-08-06 05:49:58 +0200

    Task 4.3

---
+++ src/EventList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Event from './Event';
+
+class EventList extends Component {
+  render() {
+    return (
+      <ul className="EventList">
+      </ul>
+    );
+  }
+}
+
+export default EventList;
