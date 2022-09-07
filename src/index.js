9a372ed54e34ad8ec835abe6489d147c734219fb
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-09-07 23:57:26 +0200

    Added Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('dd8911c327d14944980ba8e06b41e122').install();
+
 const container = document.getElementById('root');
 const root = createRoot (container);
 root.render(<App tab="home" />);
