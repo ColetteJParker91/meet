26c15d7f412bbd4000e46505c37fe7161865d71c
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-09-17 18:08:44 +0200

    Updated url end points
    Updated api.js to fix connection error

--- src/api.js
+++ src/api.js
@@ -2,15 +2,89 @@ import axios from 'axios';
 import NProgress from 'nprogress';
 import './nprogress.css';
 
-import { mockData } from './mock-data';
+/*import { mockData } from './mock-data'; */
 
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+ export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+  };
+  
+  /**
+ * 
+ * @param {*} accessToken : access token obtained from local storage in getAccessToken function
+ * @returns true or false depending of the token is still valid
+ * 
+ * This function uses the googleapis api to check if an access token is still valid
+ */
+
+   export const checkToken = async (accessToken) => {
+      const result = await fetch(
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`)
+      .then((res) => res.json())
+      .catch((err) => err.json());
+  
+  return result;
+      }
+
+  export const getEvents = async () => {
+        NProgress.start();
+      
+          if (window.location.href.startsWith("http://localhost")) {
+              NProgress.done();
+              return mockData;
+          }
+          // If the user is offline, return the list of events previously stored in the localStorage
+          if (!navigator.onLine) {
+            console.log('Im offline!');
+            const data = localStorage.getItem("lastEvents");
+            NProgress.done();
+            return data ? JSON.parse(data).events : [];
+        }
+          const token = await getAccessToken();
+      
+          if (token) {
+            removeQuery();
+            const url = 'https://rgwvcr54y1.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+            const result = await axios.get(url);
+            if (result.data) {
+              var locations = extractLocations(result.data.events);
+              localStorage.setItem("lastEvents", JSON.stringify(result.data));
+              localStorage.setItem("locations", JSON.stringify(locations));
+            }
+            NProgress.done();
+            return result.data.events;
+          }
+        };
+
+        const removeQuery = () => {
+          if (window.history.pushState && window.location.pathname) {
+            var newurl =
+              window.location.protocol +
+              "//" +
+              window.location.host +
+              window.location.pathname;
+            window.history.pushState("", "", newurl);
+          } else {
+            newurl = window.location.protocol + "//" + window.location.host;
+            window.history.pushState("", "", newurl);
+          }
+        };
 
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
     'https://rgwvcr54y1.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
-    "/"  + encodeCode
-  )
+    "/"  + encodeCode)
+  
       .then((res) => {
           return res.json();
       })
@@ -19,35 +93,12 @@ const getToken = async (code) => {
   access_token && localStorage.setItem("access_token", access_token);
 
   return access_token;
-};
+}
 
-export const checkToken = async (accessToken) => {
-  try {
-    const result = await fetch(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    );
-    return await result.json();
-  } catch (error) {
-    error.json();
-  }
-};
-
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
+
   const tokenCheck = accessToken && (await checkToken(accessToken));
   
   if (!accessToken || tokenCheck.error) {
@@ -64,52 +115,4 @@ export const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
-}
-
-
-export const getEvents = async () => {
-  NProgress.start();
-
-    if (window.location.href.startsWith("http://localhost")) {
-        NProgress.done();
-        return mockData;
-    }
-    if (!navigator.onLine) {
-        const data = localStorage.getItem("lastEvents");
-        NProgress.done();
-        return data 
-            ? JSON.parse(data).events 
-            : [];
-    }
-    const token = await getAccessToken();
-
-    if (token) {
-      removeQuery();
-      const url = 'https://rgwvcr54y1.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
-      const result = await axios.get(url);
-      if (result.data) {
-        var locations = extractLocations(result.data.events);
-        localStorage.setItem("lastEvents", JSON.stringify(result.data));
-        localStorage.setItem("locations", JSON.stringify(locations));
-      }
-      NProgress.done();
-      return result.data.events;
-    }
-  };
-  
-
-
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
- export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-  };
-  
+};
