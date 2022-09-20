13d0eb1cb15de28b1a85d67431cebe0b6fb73523
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-09-20 18:13:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,106 +1,100 @@
-import React, { Component } from 'react';
-
-import EventList from './EventList';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from "./EventList";
 import EventGenre from "./EventGenre";
-import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-
-import WelcomeScreen from './WelcomeScreen';
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
+import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import { OffLineAlert } from "./Alert";
-
-import './App.css';
-import './nprogress.css';
-import { 
-  ScatterChart, 
-  Scatter, 
-  XAxis, 
+import "./nprogress.css";
+import WelcomeScreen from "./WelcomeScreen";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
   YAxis,
-  CartesianGrid, 
-  Tooltip, 
-  ResponsiveContainer,} from "recharts";
-
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    locationSelected: "all",
+    showWelcomeScreen: undefined,
+  };
 
-    constructor() {
-        super();
-        this.state = {
-            events: [],
-            locations: [],
-            numberOfEvents: 32,
-            selectedLocation: 'all',
-            showWelcomeScreen: undefined
-        }
-    };
-
-
-    async componentDidMount() {
-        this.mounted = true;
-        const accessToken = localStorage.getItem('access_token');
-        const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-        const searchParams = new URLSearchParams(window.location.search);
-        const code = searchParams.get("code");
-        this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-        if ((code || isTokenValid) && this.mounted) {
-        getEvents().then((events) => {
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
         if (this.mounted) {
-        this.setState({ events,
-             locations: extractLocations(events),
-             });
+          let sliceNumber = this.state.numberOfEvents;
+          this.setState({
+            locations: extractLocations(events),
+            events: events.slice(0, sliceNumber),
+          });
         }
-        });
-        }
-        }
-
-    componentWillUnmount() {
-        this.mounted = false;
+      });
     }
+  }
 
-  updateEvents = (location, eventCount) => {
-    if (eventCount === undefined) { eventCount = this.state.numberOfEvents }
-        if (location === undefined) { location = this.state.selectedLocation }
+  componentWillUnmount() {
+    this.mounted = false;
+  }
 
-        getEvents().then((events) => {
-            let locationEvents = (location === 'all') ? events
-                : events.filter((event) => event.location === location);
+  updateEvents = (location, maxNumberEvents) => {
+    if (maxNumberEvents === undefined) {
+      maxNumberEvents = this.state.numberOfEvents;
+    } else this.setState({ numberOfEvents: maxNumberEvents });
+    if (location === undefined) {
+      location = this.state.locationSelected;
+    }
+    getEvents().then((events) => {
+      let locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents.slice(0, maxNumberEvents),
+        numberOfEvents: maxNumberEvents,
+        locationSelected: location,
+      });
+    });
+  };
 
-            this.setState({
-                events: locationEvents.slice(0, eventCount),
-                numberOfEvents: eventCount,
-                selectedLocation: location
-            });
-        });
-    };
+  updateNumberEvents = (numberOfEvents) => {
+    this.setState({
+      numberOfEvents,
+    });
+    this.updateEvents(undefined, numberOfEvents);
+  };
 
-    updateNumberEvents = (numberOfEvents) => {
-        this.setState({
-          numberOfEvents,
-        });
-        this.updateEvents(undefined, numberOfEvents);
-      };
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
 
-      getData = () => {
-        const { locations, events } = this.state;
-        const data = locations.map((location) => {
-          const number = events.filter(
-            (event) => event.location === location
-          ).length;
-          const city = location.split(", ").shift();
-          return { city, number };
-        });
-        return data;
-      };
-
-
-    render() {
-       /* const { showWelcomeScreen, events, locations, numberOfEvents,} = this.state; */
-
-        if (this.state.showWelcomeScreen === undefined)
-        return <div className="App" />;
+  render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
       <div className="App">
-        <div className="navbar"></div>
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
@@ -128,8 +122,10 @@ class App extends Component {
             <EventGenre events={this.state.events} />
           </div>
           <div className="scatter-wrapper">
-            <ResponsiveContainer width={400} height={400}>
+            <ResponsiveContainer>
               <ScatterChart
+                width={400}
+                height={400}
                 margin={{
                   top: 20,
                   right: 20,
@@ -145,6 +141,7 @@ class App extends Component {
                   name="Number of events"
                   allowDecimals={false}
                 />
+
                 <Tooltip cursor={{ strokeDasharray: "3 3" }} />
                 <Scatter data={this.getData()} fill="#8884d8" />
               </ScatterChart>
@@ -156,5 +153,5 @@ class App extends Component {
     );
   }
 }
-  
+
 export default App;
